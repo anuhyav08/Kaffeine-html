34e2151a90a1c27741b2490c3b4f772fb5f6f63c
Anuhya Vyasam <anuhya.30490@gmail.com>
2019-09-08 20:07:34 -0400

    coffee-shop website implementation

---
+++ src/coffeeshop.js
@@ -0,0 +1,8 @@
+$(document).ready(() => {
+    $('.menu-button').on('mouseenter', () => {
+        $('.nav-menu').show()
+      })
+      $('.nav-menu').on('mouseleave', () => {
+        $('.nav-menu').hide();
+      })
+    })
